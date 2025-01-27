55c184ce854d753f4a8b9b46c3fba10eb21e28b2
VerdDed <155309758+VerdDed@users.noreply.github.com>
2025-01-27 22:51:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        switch (a){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            default:
+        }
+    }
+}
